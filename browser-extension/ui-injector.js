f2ef4cc331a491d0909c9e938dd6c10a88b7d9d7
haroldmei <haroldmei.cn@gmail.com>
2025-04-03 21:05:09 +1100

    Bump version to 0.1.16

--- browser-extension/ui-injector.js
+++ browser-extension/ui-injector.js
@@ -5,7 +5,7 @@
 
 (function() {
   // Version information - should match manifest.json
-  const VERSION = "0.1.15";
+  const VERSION = "0.1.16";
   
   // Check if we're in a frame - only inject in the main frame
   if (self !== self.top) return;
